d6941ce5d42e9291ccf74baa8bf891db943ea313
Doro <call.me.dorrro@gmail.com>
2016-03-28 00:37:56 +0200

    grunt has been configured

---
+++ src/CookBook/gruntfile.js
@@ -0,0 +1,18 @@
+﻿/// <binding ProjectOpened='bower:install' />
+module.exports = function (grunt) {
+    grunt.initConfig({
+        bower: {
+            install: {
+                options: {
+                    targetDir: "wwwroot/lib",
+                    layout: "byComponent",
+                    verbose: true,
+                    install: true,
+                    bowerOptions: {}
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-bower-task");
+};
