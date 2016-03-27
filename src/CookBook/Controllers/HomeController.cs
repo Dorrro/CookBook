d04f435a06c849541d5094954e42b6a4fdc27363
Doro <call.me.dorrro@gmail.com>
2016-03-27 01:37:47 +0100

    I had to create a new empty project just to understand how the MVC 6 really works - that's the result

--- src/CookBook/Controllers/HomeController.cs
+++ src/CookBook/Controllers/HomeController.cs
@@ -4,26 +4,8 @@ namespace CookBook.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: /<controller>/
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
         {
             return View();
         }
